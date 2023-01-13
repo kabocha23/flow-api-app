f59b9351cf4a663e133c2c8df26bd1f611f92a8f
Jason Kobuchi <jekobuchi@gmail.com>
2023-01-13 14:53:52 -0800

    modified import

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
-import {config} from "@onflow/fcl"
+import * as fcl from "@onflow/fcl";
 
-config()
-  .put("accessNode.api", process.env.REACT_APP_ACCESS_NODE) // Configure FCL's Access Node
-  .put("challenge.handshake", process.env.REACT_APP_WALLET_DISCOVERY) // Configure FCL's Wallet Discovery mechanism
-  .put("0xProfile", process.env.REACT_APP_CONTRACT_PROFILE) // Will let us use `0xProfile` in our Cadence
+fcl.config({
+  "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn", // Endpoint set to Testnet
+})
+
+fcl.authenticate()
